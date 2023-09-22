6ec3ec4a85ff919048c34f25946d4f6ac6622271
CedricGayraudGreenweb <gayraud854@gmail.com>
2023-09-22 11:05:21 +0200

    base du projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require('./layout.php');
+//Le fichier layout contient la connexion à la base de données ainsi qu'au cdn de tailwind
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1 class="bg-green-700">CECI EST LA PAGE INDEX</h1>
+
+
+</body>
+
+</html>
