456b5f05b837738e6bd474a91479cbcc0f8334fc
Fabien Pinel <fabien.pinel08@gmail.com>
2023-09-25 23:56:10 +0200

    ajout currentPage sideBar + lien google font

--- layout.php
+++ layout.php
@@ -10,7 +10,18 @@ try {
 ?>
 
 <head>
+    <!--Tailwind CSS-->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!--Alpine JS-->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script defer src="ressources\js\tailwind-config.js"></script>
+    <!--Google font-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <!--Carousel-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 </head>
+<!--    bleuFonce: "#364BFF",
+        bleuClair: "#31ABFF",
+        bleuPale: "#BAE1FE",
+        vert: "#2CE6C1", 
+-->
