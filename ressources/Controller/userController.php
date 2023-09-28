e3bfd7d96c1550a0fb1d92498369e67719b28d40
Jabirv <121808816+Jabirv@users.noreply.github.com>
2023-09-28 16:04:31 +0200

    Création fonction modification détails utilisateur

--- ressources/Controller/userController.php
+++ ressources/Controller/userController.php
@@ -20,4 +20,45 @@ function getUsersFromDatabase($bdd) {
 $results = getUsersFromDatabase($bdd);
 
 
+
+
+function modifyUserDetails($bdd, $id, $newData) {
+    try {
+        // Définir la requête SQL pour mettre à jour les détails de l'utilisateur
+        $query = "UPDATE users 
+                   SET 
+                      nickname = :nickname,
+                      lastName = :lastName, 
+                      firstName = :firstName, 
+                      email = :email, 
+                      role = :role, 
+                      rank = :rank, 
+                      isBanned = :isBanned 
+                  WHERE id = :id";
+        
+        // Préparer la requête SQL
+        $stmt = $bdd->prepare($query);
+        
+        // Liage des paramètres
+        $stmt->bindParam(':lastName', $newData['lastName']);
+        $stmt->bindParam(':firstName', $newData['firstName']);
+        $stmt->bindParam(':email', $newData['email']);
+        $stmt->bindParam(':role', $newData['role']);
+        $stmt->bindParam(':rank', $newData['rank']);
+        $stmt->bindParam(':isBanned', $newData['isBanned']);
+        $stmt->bindParam(':id', $id);
+        
+        // Exécuter la requête SQL
+        $stmt->execute();
+        
+        // La mise à jour a réussi
+        return true;
+    } catch (PDOException $e) {
+        // En cas d'erreur, vous pouvez choisir de la gérer ici.
+        // Par exemple, affichez un message d'erreur ou enregistrez-le dans un fichier de journal.
+        // Pour l'instant, nous retournons false en cas d'erreur.
+        return false;
+    }
+}
+
 ?>
