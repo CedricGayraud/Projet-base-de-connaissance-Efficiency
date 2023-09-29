99363b24b4058ba67d7096050a746ab23b8d87fb
marwan-b <bouchrikamarvin@gmail.com>
2023-09-29 14:17:02 +0200

    Classe(Getter/Setter) User.php, Post.php Création de fichier CommentForum.php

---
+++ ressources/Class/User.php
@@ -0,0 +1,87 @@
+<?php
+class User {
+    private int $id;
+    private string $nickname;
+    private string $lastName;
+    private string $firstName;
+    private string $email;
+    private bool $role;
+    private string $rank;
+    private bool $isBanned;
+
+    public function __construct($id, $nickname, $lastName, $firstName, $email, $role, $rank, $isBanned) {
+        $this -> id = $id;
+        $this -> nickname = $nickname;
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
+        $this->email = $email;
+        $this->role = 1;
+        $this->rank = $rank;
+        $this->isBanned = 0;
+    }
+
+    // GETTERS
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNickname() {
+        return $this->nickname;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+
+    public function getRank() {
+        return $this->rank;
+    }
+
+    public function getIsBanned() {
+        return $this->isBanned;
+    }
+
+    // SETTERS
+
+    public function setNickname($nickname) {
+        $this->nickname = $nickname;
+    }
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setRole($role) {
+        $this->role = $role;
+    }
+
+    public function setRank($rank) {
+        $this->rank = $rank;
+    }
+
+    public function setIsBanned($isBanned) {
+        $this->isBanned = $isBanned;
+    }
+
+}
--- ressources/views/profil.php
+++ ressources/views/profil.php
@@ -1,6 +1,6 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/layout.php');
-require '../Class/user.php';
+require '../Class/User.php';
 require '../Controller/userController.php';
 
 
