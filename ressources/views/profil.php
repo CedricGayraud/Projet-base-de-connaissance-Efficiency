b7e89c6c5149bb33a33c6ca5b768c44a1a78fd27
Jabirv <121808816+Jabirv@users.noreply.github.com>
2023-10-12 11:35:57 +0200

    Pop up modifier les informations

--- ressources/views/profil.php
+++ ressources/views/profil.php
@@ -52,7 +52,7 @@ $user = new user (
         </div>
     <div class="flex justify-center mt-6 border-t border-gray-100  ">
         
-        <dl class="divide-y divide-gray-100 ">
+        <dl class="divide-y divide-gray-100">
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-2 sm:px-0">
                 <dt class="font-medium leading-6 text-gray-900">Nom</dt>
                 <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"><?php echo $user->getLastName()?></dd>
@@ -67,9 +67,29 @@ $user = new user (
       </div>
     
       <div class="flex justify-center mt-6">
-            <button class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mr-4">Modifier les informations</button>
-            <button type="submit" class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enregistrer</button>
-        </div>
+        <div x-data="{ showModal: false }">
+          <div class="flex justify-center mt-6">
+              <button @click="showModal = true" class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mr-4">Modifier les informations</button>
+              <div x-show="showModal" class="fixed inset-0 flex items-center justify-center z-50" style="background-color: rgba(0, 0, 0, 0.5);">
+                  <div class="bg-white p-8 rounded-lg shadow-lg" @click.away="showModal = false">
+                     <span class="absolute top-0 right-0 p-4 cursor-pointer" @click="showModal = false">&times;</span>
+                    <form action="register_controller.php" method="post" action="_URL_" enctype="multipart/form-data">
+                    <label for="nickname" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Pseudo</label>
+            <input type="text" name="nickname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring focus:ring-[#BAE1FE]" placeholder="Votre pseudo" required>
+            <label for="lastName" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Nom</label>
+            <input type="text" name="lastname" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring focus:ring-[#BAE1FE]" placeholder="Votre nom" required>
+            <label for="firstName" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Prenom</label>
+            <input type="text" name="firstname" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring focus:ring-[#BAE1FE]" placeholder="Votre prénom" required>
+            <label for="email" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Email</label>
+            <input type="email" name="email" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring focus:ring-[#BAE1FE]" placeholder="Votre E-mail" required>
+            <label for="password" class="mt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Mot de passe</label>
+            <input type="password" name="password" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring focus:ring-[#BAE1FE]" placeholder="Votre mot de passe" required>
+            <button type="submit" class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 mt-6 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enregistrer</button>
+            </form>
+    </div>
+  </div>
+</div>
+
 
 </body>
 
