77f61bec5401c6d35aa4e2149f94f5153a7c3a91
Fabien Pinel <fabien.pinel08@gmail.com>
2023-10-22 12:54:48 +0200

    fix espace champ description

--- ressources/views/creation_platform.php
+++ ressources/views/creation_platform.php
@@ -13,8 +13,7 @@ include('./sidebar.php');
             </div>
             <div class="mt-2">
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                <textarea type="text" name="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-[#BAE1FE] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre description" required>
-            </textarea>
+                <textarea type="text" name="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-[#BAE1FE] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre description" required></textarea>
             </div>
             <div class="mt-2">
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Lien</label>
