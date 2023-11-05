cfc8a8d2fdbc34c875c58fded8f3d48c3097e78d
Fabien Pinel <fabien.pinel08@gmail.com>
2023-11-05 18:05:00 +0100

    Fiche : Ajustement css + count des commentaires

--- ressources/views/fiche.php
+++ ressources/views/fiche.php
@@ -6,40 +6,29 @@ require './session_config.php';
 
 $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
 $id_card = $_GET['fiche'];
+
+$card = Card::getCardById($id_card);
+$likes = CardLike::getAllLikesByCardId($id_card);
+$comments = Comment::getAllCommentsByCardId($id_card);
+if (isset($_SESSION['user'])) {
+    $sessionUser = User::getSessionUser($bdd);
+    $sessionUserId = $sessionUser->getId();
+    $isLiked = CardLike::isLiked($id_card, $sessionUserId);
+}
+$countComments = Comment::countCommentsByCardId($id_card);
+
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/path/to/your/generated/css/tailwind.css">
-
+    <title>Efficiency - <?php echo $card->getTitle() ?></title>
+    <link rel="icon" type="image/x-icon" href="https://image.noelshack.com/fichiers/2023/39/3/1695821591-logo-efficiency.png" />
 </head>
 
 <body class="bg-gray-100">
-    <?php
-
-    $sessionUser = User::getSessionUser($bdd);
-    $card = Card::getCardById($id_card);
-    $likes = CardLike::getAllLikesByCardId($id_card);
-
-
-    $comments = Comment::getAllCommentsByCardId($id_card);
-    if (isset($_SESSION['user'])) {
-        $sessionUserId = $sessionUser->getId();
-        $isLiked = CardLike::isLiked($id_card, $sessionUserId);
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['content'])) {
-            // Récupérez les données du formulaire
-            $content = $_POST['content'];
-
-            // Créez le commentaire en utilisant $content
-            Comment::commentCreate($content, $sessionUserId, $id_card);
-        }
-    }
-
-    ?>
     <?php include 'sidebar.php' ?>
     <div class="md:ml-28 md:mr-8">
         <div class="flex justify-center mt-10">
@@ -76,11 +65,24 @@ $id_card = $_GET['fiche'];
                 </button>
 
             </form>
-            <div class="mt-4 text-center">
-                <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+            <?php foreach ($comments as $comment) : ?>
+                <div class="max-w-2xl mx-auto px-4 mt-4">
+                    <article class="p-6 text-base bg-white rounded-lg shadow-lg dark:bg-gray-900">
+                        <footer class="flex justify-between items-center mb-2">
+                            <div class="flex items-center">
+                                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img class="mr-2 w-6 h-6 rounded-full" src="<?= $comment->getUser()->getProfilPicture(); ?>" alt="Michael Gough"><?= $comment->getUser()->getNickname(); ?></p>
+                                <p class="text-sm text-gray-600 dark:text-gray-400">Le <?= formatDateDay($comment->getCreatedDate()); ?></p>
+                            </div>
+                        </footer>
+                        <p class="text-gray-500 dark:text-gray-400"><?= $comment->getContent(); ?></p>
+                    </article>
+                </div>
+            <?php endforeach; ?>
+            <div class="mt-2 text-center">
+                <section class="dark:bg-gray-900 py-8 lg:py-8 antialiased">
                     <div class="max-w-2xl mx-auto px-4">
                         <div class="flex justify-between items-center mb-6">
-                            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (20)</h2>
+                            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (<?php echo $countComments ?>)</h2>
                         </div>
                         <form action="../Controller/commentController.php" method="post">
                             <input type="hidden" name="card_id" value="<?php echo $card->getId(); ?>">
@@ -93,38 +95,14 @@ $id_card = $_GET['fiche'];
                                 Envoyer
                             </button>
                         </form>
-
-
-
-
-
-
-
-
                     </div>
-                    </article>
+                </section>
             </div>
-            </section>
+        <?php } ?>
     </div>
-<?php }
-        foreach ($comments as $comment) : ?>
-    <div class="mt-4 text-center">
-        <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
-            <div class="max-w-2xl mx-auto px-4">
-                <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
-                    <footer class="flex justify-between items-center mb-2">
-                        <div class="flex items-center">
-                            <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img class="mr-2 w-6 h-6 rounded-full" src="<?= $comment->getUser()->getProfilPicture(); ?>" alt="Michael Gough"><?= $comment->getUser()->getNickname(); ?></p>
-                            <p class="text-sm text-gray-600 dark:text-gray-400"><?= $comment->getCreatedDate(); ?></p>
-                        </div>
-                    </footer>
-                    <p class="text-gray-500 dark:text-gray-400"><?= $comment->getContent(); ?></p>
-                </article>
-            </div>
-        </section>
     </div>
-<?php endforeach; ?>
-</div>
 </body>
+
+<?php include('../views/footer.php'); ?>
 
 </html>
