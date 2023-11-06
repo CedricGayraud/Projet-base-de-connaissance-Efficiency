6b03dbc27323cd05a2797a098f9e96d17f67b6c7
Cedric Gayraud <gayraud854@gmail.com>
2023-11-06 15:04:33 +0100

    Copier coller code

--- ressources/views/fiche.php
+++ ressources/views/fiche.php
@@ -81,60 +81,91 @@ $countComments = Comment::countCommentsByCardId($id_card);
         <div class="mt-8 text-center">
             <p class="text-lg"><?php echo html_entity_decode($card->getContentText()); ?></p>
         </div>
-        <div class="block m-auto p-5 mt-8 text-center bg-black rounded-lg w-3/5 justify-center">
-            <p class="text-white"><?php echo $card->getGitHub() ?></p>
-        </div>
-        <?php foreach ($comments as $comment) : ?>
-            <div class="max-w-2xl mx-auto px-4 mt-4">
-                <article class="p-6 text-base bg-white rounded-lg shadow-lg dark:bg-gray-900">
-                    <footer class="flex justify-between items-center mb-2">
-                        <div class="flex items-center">
-                            <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                                <img class="mr-2 w-6 h-6 rounded-full" src="<?= $comment->getUser()->getProfilPicture(); ?>" alt="Michael Gough">
-                                <?= $comment->getUser()->getNickname(); ?>
-                            </p>
-                            <p class="text-sm text-gray-600 dark:text-gray-400">Le <?= formatDateDay($comment->getCreatedDate()); ?></p>
-                        </div>
-                        <?php if (isset($_SESSION['user']) && $sessionUser->getRole() == 1) { ?>
+
+        <div x-data="{ copied: false }" class="w-3/5 block m-auto">
+            <div class="bg-black rounded-lg">
+                <div class="flex rounded-t-lg bg-slate-600">
+                    <button class="flex ml-auto items-center px-2 text-white font-bold text-lg" @click="copyToClipboard()">
+                        <span class="text-white block ml-auto p-2 material-symbols-outlined">
+                            content_copy
+                        </span>Copier
+                    </button>
+                </div>
+                <div class="p-5 text-center">
+                    <input type="text" value="<?php echo $card->getGitHub() ?>" id="myInput" class="text-white bg-black w-full" readonly>
+                </div>
+            </div>
+
+            <template x-if="copied">
+                <div class="bg-green-300 text-green-900 p-2 mt-2 rounded">Copié dans le presse-papiers!</div>
+            </template>
+
+
+            <?php foreach ($comments as $comment) : ?>
+                <div class="max-w-2xl mx-auto px-4 mt-4">
+                    <article class="p-6 text-base bg-white rounded-lg shadow-lg dark:bg-gray-900">
+                        <footer class="flex justify-between items-center mb-2">
+                            <div class="flex items-center">
+                                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                                    <img class="mr-2 w-6 h-6 rounded-full" src="<?= $comment->getUser()->getProfilPicture(); ?>" alt="Michael Gough">
+                                    <?= $comment->getUser()->getNickname(); ?>
+                                </p>
+                                <p class="text-sm text-gray-600 dark:text-gray-400">Le <?= formatDateDay($comment->getCreatedDate()); ?></p>
+                            </div>
+                            <?php if (isset($_SESSION['user']) && $sessionUser->getRole() == 1) { ?>
+                                <form action="../Controller/commentController.php" method="post">
+                                    <input type="hidden" name="comment_id" value="<?php echo $comment->getId(); ?>">
+                                    <input type="hidden" name="card_id" value="<?php echo $card->getId(); ?>">
+                                    <button type="submit" name="delete_Comment" class="text-white bg-red-500 hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm ml-auto px-5 py-2.5 text-center">
+                                        X
+                                    </button>
+                                </form>
+                            <?php } ?>
+                        </footer>
+                        <p class="text-gray-500 dark:text-gray-400"><?= $comment->getContent(); ?></p>
+                    </article>
+                </div>
+            <?php endforeach; ?>
+            <?php
+            if (isset($_SESSION['user'])) {
+            ?>
+                <div class="mt-2 text-center">
+                    <section class="dark:bg-gray-900 py-8 lg:py-8 antialiased">
+                        <div class="max-w-2xl mx-auto px-4">
+                            <div class="flex justify-between items-center mb-6">
+                                <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (<?php echo $countComments ?>)</h2>
+                            </div>
                             <form action="../Controller/commentController.php" method="post">
-                                <input type="hidden" name="comment_id" value="<?php echo $comment->getId(); ?>">
                                 <input type="hidden" name="card_id" value="<?php echo $card->getId(); ?>">
-                                <button type="submit" name="delete_Comment" class="text-white bg-red-500 hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm ml-auto px-5 py-2.5 text-center">
-                                    X
+                                <input type="hidden" name="user_id" value="<?php echo $sessionUserId; ?>">
+                                <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                                    <label for="comment" class="sr-only">Votre commentaire</label>
+                                    <textarea name="content" rows="6" class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800" placeholder="Écrire un commentaire..." required></textarea>
+                                </div>
+                                <button type="submit" name="create_Comment" class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                                    Envoyer
                                 </button>
                             </form>
-                        <?php } ?>
-                    </footer>
-                    <p class="text-gray-500 dark:text-gray-400"><?= $comment->getContent(); ?></p>
-                </article>
-            </div>
-        <?php endforeach; ?>
-        <?php
-        if (isset($_SESSION['user'])) {
-        ?>
-            <div class="mt-2 text-center">
-                <section class="dark:bg-gray-900 py-8 lg:py-8 antialiased">
-                    <div class="max-w-2xl mx-auto px-4">
-                        <div class="flex justify-between items-center mb-6">
-                            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (<?php echo $countComments ?>)</h2>
                         </div>
-                        <form action="../Controller/commentController.php" method="post">
-                            <input type="hidden" name="card_id" value="<?php echo $card->getId(); ?>">
-                            <input type="hidden" name="user_id" value="<?php echo $sessionUserId; ?>">
-                            <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                                <label for="comment" class="sr-only">Votre commentaire</label>
-                                <textarea name="content" rows="6" class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800" placeholder="Écrire un commentaire..." required></textarea>
-                            </div>
-                            <button type="submit" name="create_Comment" class="text-white bg-[#2CE6C1] hover:bg-[#BAE1FE] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                                Envoyer
-                            </button>
-                        </form>
-                    </div>
-                </section>
-            </div>
-        <?php } ?>
+                    </section>
+                </div>
+            <?php } ?>
+        </div>
     </div>
-    </div>
+
+    <script>
+        function copyToClipboard() {
+            var copyText = document.getElementById("myInput");
+            copyText.select();
+            document.execCommand("copy");
+
+            this.copied = true;
+            setTimeout(() => {
+                this.copied = false;
+            }, 2000);
+        }
+    </script>
+
 </body>
 
 <?php include('../views/footer.php'); ?>
