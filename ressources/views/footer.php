2772b94418394a1fcce8ba49c0f0033f28bd688c
Fabien Pinel <fabien.pinel08@gmail.com>
2023-11-09 19:43:04 +0100

    Footer : Fix taille input

--- ressources/views/footer.php
+++ ressources/views/footer.php
@@ -15,7 +15,7 @@
                 <form class="mt-2" action="ressources/views/footer_controller.php" method="post">
                     <div class="sm:flex sm:max-w-md">
                         <input type="email" name="email" autocomplete="email" required class="mr-2 w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full my-2" placeholder="Votre email">
-                        <input type="text" name="name" autocomplete="name" required class="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full mb-2" placeholder="Votre nom">
+                        <input type="text" name="name" autocomplete="name" required class="mr-2 w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full my-2" placeholder="Votre nom">
                     </div>
                     <div class="sm:flex sm:max-w-md">
                         <input type="text" name="message" id="message" required class="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Votre message">
